18570b6dbb57156e6a4457e77a637b4932dd6b39
Мария Цалта-Цветкова <code-destroyer@users.noreply.github.com>
2015-02-20 19:44:34 +0200

    Update fill_tetrahedron_with_water.py

--- fill_tetrahedron_with_water.py
+++ fill_tetrahedron_with_water.py
@@ -2,10 +2,11 @@ import math
 
 def fill_tetrahedron(num):
     if type(num)==int or num%1==0:
-        Volume_cm=(num**3)/(6*(math.sqrt(2)))
-        Volume_L=Volume_cm/1000
-        print (" %.2f" % Volume_L)
-        return
-    else: print("Ne e cqlo chislo")
+        Volume=(num**3)/(6*(math.sqrt(2)))
+        liters=Volume/1000
+        print (" %.2f" % liters)
+        return liters
+    else:
+        raise ValueError('%s is not integer number' %str(num))
 
 fill_tetrahedron(100)
